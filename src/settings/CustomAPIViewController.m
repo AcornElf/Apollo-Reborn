c91ff45a49f739714339f39a00bb505a005dd6f3
AcornElf <nedfalconer+acornelf.github@gmail.com>
2026-09-11 17:16:23 +0100

    Update Info Row summary labels

--- src/settings/CustomAPIViewController.m
+++ src/settings/CustomAPIViewController.m
@@ -1639,18 +1639,18 @@ typedef NS_ENUM(NSInteger, Tag) {
         [parts addObject:@"Magnifier"];
     }
 
+    if (sInfoRowTapUpvote) {
+        [parts addObject:@"Upvote on Release"];
+    }
+
     if (sInfoRowOverlayMode) {
         [parts addObject:@"Timestamp Overlay"];
     } else if (sInfoRowPopupMode) {
-        [parts addObject:@"Timestamp Pop-up"];
-    }
-
-    if (sInfoRowTapUpvote) {
-        [parts addObject:@"Upvote"];
+        [parts addObject:@"Timestamp Pop-Up"];
     }
 
     if (sInfoRowTapComments) {
-        [parts addObject:@"Comments"];
+        [parts addObject:@"Jump to Comments"];
     }
 
     BOOL translationAvailable = sTapToTranslate || sShowTranslationTitleDetails || sShowTranslationDetails;
